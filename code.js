17d56d6d2abf6eb1da56ebcb86b648dbd2a814b4
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-02-07 11:41:00 -0600

    complete exercise 10

--- code.js
+++ code.js
@@ -10,10 +10,16 @@ function sayHello(input){
         return "Hello, World!";
     } else if (parseInt(input) === Number(input)) {
         return "Hello, World!";
-    } else if (typeof input === "string" && parseInt(input) !==  NaN){
+    } else if (typeof input === "string"){
         return "Hello, " + input + "!";
     } else {
         return "Hello, World!";
     }
 }
+
+//isFive function, exercise #10
+function isFive(input){
+    return input == 5;
+}
+
 
--- tests.js
+++ tests.js
@@ -53,8 +53,17 @@ describe('sayHello', function(){
     });
 });
 
-describe("isEven", function(){
+describe("isFive", function(){
     it('should be a defined function', function () {
-        expect(typeof sayHello).toBe("function");
+        expect(typeof isFive).toBe("function");
+    });
+    it('should return a boolean', function () {
+        expect(typeof isFive()).toBe("boolean");
+    });
+    it('should return true when given the number 5', function () {
+        expect(isFive(5)).toBe(true);
+    });
+    it('should return true when given "5"', function () {
+        expect(isFive("5")).toBe(true);
     });
 })
